39429cf6afced881fd89e079d4895633409020f4
LazyMan78 <demetra_n@front.ru>
2021-01-05 15:49:32 +0300

    add custom detail view for order in admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -2,9 +2,15 @@ import csv
 import datetime
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 # Register your models here.
+
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(
+        reverse('orders:admin_order_detail', args=[obj.id])))
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -32,8 +38,10 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
+
+
 
