ac7f94f93b30e32f0f81795d5326633d6fab5ddd
LazyMan78 <demetra_n@front.ru>
2021-01-05 16:30:26 +0300

    generate invoice to pdf and attach to mail

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,11 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.shortcuts import render, get_object_or_404, redirect
 import braintree
+import weasyprint
+from io import BytesIO
+
+from django.template.loader import render_to_string
 from orders.models import Order
 # Create your views here.
 
@@ -20,6 +26,20 @@ def payment_process(request):
             order.paid = True
             order.braintree_id = result.transaction.id
             order.save()
+            subject = f'My Shop - Invoice no. {order.id}'
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'admin@myshop.com',
+                                 [order.email])
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+            email.attach(f'order_{order.id}.pdf',
+                         out.getvalue(),
+                         'application/pdf')
+            email.send()
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
